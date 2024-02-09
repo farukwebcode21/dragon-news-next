2306aeef401fe653e340e7cf6698ec62ed1f6be4
farukwebcode21 <farukahmadmk@yahoo.com>
2024-02-09 18:45:10 +0600

    sidebar news complete

--- src/components/ui/SideBar/SideBar.js
+++ src/components/ui/SideBar/SideBar.js
@@ -5,8 +5,8 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import TopNews from "@/assets/top-news.png";
-import TopNews2 from "@/assets/top-news2.png";
+import TopNews from "@/assets/side-bottom-img.png";
+import TopNews2 from "@/assets/side-top-news.png";
 import Image from "next/image";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -14,10 +14,9 @@ import Grid from "@mui/material/Grid";
 const SideBarPages = () => {
   return (
     <div>
-      {" "}
       <Card className="my-2">
         <CardMedia>
-          <Image src={TopNews} alt="TopNews" />
+          <Image src={TopNews2} alt="TopNews" />
         </CardMedia>
         <CardContent>
           <p className="text-white w-[1000px] bg-red-600 px-1  underline rounded">
@@ -26,12 +25,87 @@ const SideBarPages = () => {
           <Typography component="div">
             Bitcoin Climbs as Elon Musk Says Tesla Likely to accept it again
           </Typography>
-          <Typography
-            className="bg-red-600 w-[220px] px-1 text-white"
-            gutterBottom
-            variant="body2"
-            component="div"
-          >
+          <Typography className="text-xs px-1">
+            By Faruk Ahmad -Feb 09 2024
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card className="my-2">
+        <CardMedia>
+          <Image
+            className="h-[150px] object-cover"
+            src={TopNews}
+            alt="TopNews"
+          />
+        </CardMedia>
+        <CardContent>
+          <p className="text-white w-[1000px] bg-red-600 px-1  underline rounded">
+            Technology
+          </p>
+          <Typography component="div">
+            Bitcoin Climbs as Elon Musk Says Tesla Likely to accept it again
+          </Typography>
+          <Typography className="text-xs px-1">
+            By Faruk Ahmad -Feb 09 2024
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card className="my-2">
+        <CardMedia>
+          <Image src={TopNews2} alt="TopNews" />
+        </CardMedia>
+        <CardContent>
+          <p className="text-white w-[1000px] bg-red-600 px-1  underline rounded">
+            Technology
+          </p>
+          <Typography component="div">
+            Bitcoin Climbs as Elon Musk Says Tesla Likely to accept it again
+          </Typography>
+          <Typography className="text-xs px-1">
+            By Faruk Ahmad -Feb 09 2024
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card className="my-2">
+        <CardMedia>
+          <Image
+            className="h-[150px] object-cover"
+            src={TopNews}
+            alt="TopNews"
+          />
+        </CardMedia>
+        <CardContent>
+          <p className="text-white w-[1000px] bg-red-600 px-1  underline rounded">
+            Technology
+          </p>
+          <Typography component="div">
+            Bitcoin Climbs as Elon Musk Says Tesla Likely to accept it again
+          </Typography>
+          <Typography className="text-xs px-1">
             By Faruk Ahmad -Feb 09 2024
           </Typography>
           <Typography variant="body2" color="text.secondary">
